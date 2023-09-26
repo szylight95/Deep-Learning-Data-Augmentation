857d0f10c4e715c81edf0bcefb53a5dd3ec9963b
szylight95 <szylight@163.com>
2023-09-26 11:31:18 +0800

    feat:add cutout()

---
+++ cutout.py
@@ -0,0 +1,17 @@
+import random
+
+# Applies image cutout augmentation https://arxiv.org/abs/1708.04552
+def cutout(img):
+    h, w = img.shape[:2]
+    scales = [0.5] * 1 + [0.25] * 2 + [0.125] * 4 + [0.0625] * 8 + [0.03125] * 16
+    for s in scales:
+        mask_h = random.randint(1, int(h * s))
+        mask_w = random.randint(1, int(w * s))
+
+        # box
+        xmin = max(0, random.randint(0, w) - mask_w // 2)
+        ymin = max(0, random.randint(0, h) - mask_h // 2)
+        xmax = min(w, xmin + mask_w)
+        ymax = min(h, ymin + mask_h)
+
+        img[ymin:ymax, xmin:xmax] = [random.randint(0, 255) for _ in range(3)]
